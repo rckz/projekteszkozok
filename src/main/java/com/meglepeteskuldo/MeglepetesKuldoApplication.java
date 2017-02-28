32ffb96ce8f91d8dc5b5004553497edd7917c408
Krisztian Harkai <krisz.harkai@gmail.com>
2017-02-28 19:11:44 +0100

    awfwaf

--- src/main/java/com/meglepeteskuldo/MeglepetesKuldoApplication.java
+++ src/main/java/com/meglepeteskuldo/MeglepetesKuldoApplication.java
@@ -3,6 +3,7 @@ package com.meglepeteskuldo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//FASZ
 @SpringBootApplication
 public class MeglepetesKuldoApplication {
 
